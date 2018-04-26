6217ce7e3713b8b46d702810ba3fcdc8ff681feb
Cédric Lautard <lautard.cedric@qenvi.com>
2018-04-26 14:50:59 +0200

    login + test cucumber

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import  ConnextionForm  from './private/common/auth/connexion/component/form';
 
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ConnextionForm email={""} password={""}/>
+        <ConnextionForm urlToFetch={'http://192.168.10.11:8080/reactBackend/connexion/login.php'}/>
       </View>
     );
   }
